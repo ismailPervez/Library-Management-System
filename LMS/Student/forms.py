616da9a952f5f77a1dc7a0f6513bc4afb51a2aba
ismailPervez <freelanceismail001@gmail.com>
2022-02-20 23:49:20 +0300

    added some styles to the student registration form

--- LMS/Student/forms.py
+++ LMS/Student/forms.py
@@ -1,7 +1,27 @@
-from django.forms import ModelForm
+from django.forms import CharField, EmailInput, ModelForm, TextInput
 from .models import Student
 
 class StudentRegistrationForm(ModelForm):
+    first_name = CharField(widget=TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'e.g John'
+    }))
+
+    last_name = CharField(widget=TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'e.g Doe'
+    }))
+
+    student_ID = CharField(widget=TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'e.g 1673827'
+    }))
+
+    email = CharField(widget=EmailInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'e.g johndoe@gmail.com'
+    }))
+
     class Meta:
         model = Student
         fields = ['first_name', 'last_name', 'student_ID', 'email']

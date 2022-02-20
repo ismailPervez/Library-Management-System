cfa9d0f7c5d3544523f8df77c4c21ed96021fbde
ismailPervez <freelanceismail001@gmail.com>
2022-02-20 22:43:31 +0300

    added a register page in the Library app that display the options in which to register

--- LMS/Library/urls.py
+++ LMS/Library/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='library-home'),
+    path('register/', views.register, name='library-register'),
     path('librarian/', include('Librarian.urls')),
     path('student/', include('Student.urls')),
 ]
--- LMS/Library/views.py
+++ LMS/Library/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'Library/index.html')
+
+def register(request):
+    return render(request, 'Library/register.html')

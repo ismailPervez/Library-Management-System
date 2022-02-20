c3003b477bf9f90a8a2b44dff05bdceda16213b9
ismailPervez <freelanceismail001@gmail.com>
2022-02-20 22:52:23 +0300

    added a simple student model

--- LMS/Student/models.py
+++ LMS/Student/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    first_name = models.CharField(max_length=30, null=False, blank=False)
+    first_name = models.CharField(max_length=30, null=False, blank=False)
+    student_ID = models.CharField(max_length=30, null=False, blank=False)
+    email = models.EmailField()

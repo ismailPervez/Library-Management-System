54673825923ef9e8ec4c21aaadebf0ebfd1d0db0
ismailPervez <freelanceismail001@gmail.com>
2022-02-22 14:54:37 +0300

    created a wishlist model

--- LMS/Library/models.py
+++ LMS/Library/models.py
@@ -2,16 +2,16 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
 
-class Book(models.Model):
-    title = models.CharField(max_length=150)
-    desc = models.TextField(max_length=150)
-    in_store = models.IntegerField()
-
 class BaseUser(AbstractUser):
     pass
 
 # Due to circular imports
 from Student.models import Student
+
+class Book(models.Model):
+    title = models.CharField(max_length=150)
+    desc = models.TextField(max_length=150)
+    in_store = models.IntegerField()
 
 class TakenBook(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
@@ -19,3 +19,7 @@ class TakenBook(models.Model):
     date_taken = models.DateTimeField(default=timezone.now)
     date_to_be_returned = models.DateTimeField()
     date_returned = models.DateTimeField(null=True)
+
+class WishList(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)

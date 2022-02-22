476c75507a76dacaf2b0f27cb536b16f70521ce5
ismailPervez <freelanceismail001@gmail.com>
2022-02-22 09:23:58 +0300

    added an add book functionality

--- LMS/Librarian/api/views.py
+++ LMS/Librarian/api/views.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from Librarian.models import Librarian
+from Library.models import Book
 
 @api_view(['PUT'])
 def update_user(request, email):
@@ -28,3 +29,21 @@ def update_user(request, email):
         return Response({'msg': 'user updated successfully'}, status=status.HTTP_200_OK)
 
     return Response({'msg': 'method not allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+@api_view(['POST'])
+def add_book(request):
+    if request.method == 'POST':
+        if not request.data:
+            return Response({'msg': 'not data found'}, status=status.HTTP_404_NOT_FOUND) 
+
+        new_book = Book(
+            title=request.data['title'],
+            desc=request.data['desc'],
+            in_store=int(request.data['in_store'])
+        )
+        new_book.save()
+        
+        return Response({'msg': 'new book has been added to the library'}, status=status.HTTP_201_CREATED)
+
+    else:
+        return Response({'msg': 'method not allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
--- LMS/Librarian/urls.py
+++ LMS/Librarian/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views 
+from Librarian.api import views as api_views
 
 urlpatterns = [
     path('', views.index, name='librarian-home'),
     path('register/', views.register, name='librarian-register'),
     path('login/', views.log_in, name='librarian-login'),
+    path('update/<email>', api_views.update_user),
+    path('add-book', api_views.add_book), # should not end with a trailing slash
 ]

48a47db5044b2a2ef21f3786cbdf48a5de128c53
ismailPervez <freelanceismail001@gmail.com>
2022-02-22 11:04:12 +0300

    fixed a circular import issue n models

--- LMS/Library/models.py
+++ LMS/Library/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from Student.models import Student
 from django.utils import timezone
 
 class Book(models.Model):
@@ -8,12 +7,15 @@ class Book(models.Model):
     desc = models.TextField(max_length=150)
     in_store = models.IntegerField()
 
+class BaseUser(AbstractUser):
+    is_staff = models.BooleanField(default=False)
+
+# Due to circular imports
+from Student.models import Student
+
 class TakenBook(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     date_taken = models.DateTimeField(default=timezone.now)
     date_to_be_returned = models.DateTimeField()
     date_returned = models.DateTimeField()
-
-class BaseUser(AbstractUser):
-    is_staff = models.BooleanField(default=False)

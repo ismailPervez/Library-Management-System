f82f3962357127d3c6f5698f89b76f46ae86c545
ismailPervez <freelanceismail001@gmail.com>
2022-02-23 17:23:57 +0300

    clear returned book functionality works well

--- LMS/Librarian/urls.py
+++ LMS/Librarian/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('update/<email>', api_views.update_user),
     path('add-book', api_views.add_book),
     path('register-taken-book', api_views.register_taken_book),
+    path('clear-returned-book', api_views.clear_returned_book),
 ]
